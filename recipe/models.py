764c335db9db92cb1b8115266e1566d0a54ef031
Subhan-Zaheer <2020cs666@student.uet.edu.pk>
2023-07-14 09:17:04 +0500

    add recipe image resolved

--- recipe/models.py
+++ recipe/models.py
@@ -5,4 +5,4 @@ from django.db import models
 class recipe(models.Model):
     recipe_name = models.CharField(max_length=50)
     recipe_desc = models.TextField()
-    recipe_image = models.ImageField(upload_to='recipe/', null=True, default=None)
+    recipe_image = models.FileField(upload_to='recipe/', max_length=250, null=True, default=None)
--- recipe/views.py
+++ recipe/views.py
@@ -5,9 +5,11 @@ from recipe.models import recipe
 
 def recipes(request):
     if request.method == 'POST':
+        print(request.POST)
         my_recipe_name = request.POST.get('recipe_name')
         my_recipe_description = request.POST.get('recipe_desc')
-        my_recipe_image = request.FILES.get('recipe_image')
+        my_recipe_image = request.FILES['recipe_image']
+        print(f'{my_recipe_name}, {my_recipe_description}, {my_recipe_image}')
         recipe_obj = recipe(recipe_name = my_recipe_name, recipe_desc= my_recipe_description, 
                             recipe_image = my_recipe_image)
         recipe_obj.save()
