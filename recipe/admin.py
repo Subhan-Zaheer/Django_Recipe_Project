2e5c63d726a785beb79bfc8e1d23e963ea62c0ab
Subhan-Zaheer <2020cs666@student.uet.edu.pk>
2023-07-17 22:58:13 +0500

    some more models created just to practice django model query.

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from recipe.models import recipe, my_user
+from recipe.models import recipe, my_user, Department, StudentId, Student
 
 # Register your models here.
 class my_recipe_admin(admin.ModelAdmin):
@@ -12,6 +12,21 @@ class my_user_admin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'user_name', 'password')
 
 admin.site.register(my_user, my_user_admin)
+
+class my_department(admin.ModelAdmin):
+    list_display = ('department_name',)
+
+admin.site.register(Department, my_department)
+
+class my_studentid(admin.ModelAdmin):
+    list_display = ('student_id', )
+
+admin.site.register(StudentId, my_studentid)
+
+class my_student(admin.ModelAdmin):
+    list_display = ('student_id', 'department', 'student_name', 'student_email', 'student_age', 'student_address')
+
+admin.site.register(Student, my_student)
 # @admin.register(recipe)
 # class recipe_admin(admin.ModelAdmin):
     # pass              
