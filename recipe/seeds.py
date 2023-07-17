c32220c0ba38d9c8ac959cce6906018cef27e783
Subhan-Zaheer <2020cs666@student.uet.edu.pk>
2023-07-17 23:30:05 +0500

    Seeds.py code done to create dump data for database.

---
+++ recipe/seeds.py
@@ -0,0 +1,32 @@
+from faker import Faker
+import random
+from .models import *
+fake = Faker()
+
+def seed_dept():
+    Department.objects.create(department_name = "Computer Sciences")
+    Department.objects.create(department_name = "Mechanical Engineering")
+    Department.objects.create(department_name = "Electrical Engineering")
+    Department.objects.create(department_name = "Civil Engineering")
+
+def seed_db():
+    for i in range(10):
+        department_obj = Department.objects.all()
+        random_index = random.randint(0, len(department_obj)-1)
+        department = random.randint(0, department_obj[random_index])
+        student_id = f"STU-{random.randint(100, 999)}"
+        student_name = fake.name()
+        student_email = fake.mail()
+        student_age = random.randint(18, 24)
+        student_address = fake.address()
+
+        student_id_obj = StudentId.objects.create(student_id = student_id)
+
+        student_obj = Student.objects.create(
+            student_name = student_name,
+            student_id = student_id_obj, 
+            department = department,
+            student_email = student_email,
+            student_age = student_age, 
+            student_address = student_address
+        )
